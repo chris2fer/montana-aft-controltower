5f09d1a44d4a71be9cc2aa1e6a8ba66b3d7d79a3
Christopher Dearie <cdearie@gmail.com>
2023-11-10 09:56:50 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ct_management_account_id" {
+  type        = string
+  description = "The Account ID for the delegated admin for Control Tower"
+}
+
+variable "log_archive_account_id" {
+  type        = string
+  description = "The Account ID for the Log Archive Account"
+}
+  
+variable "audit_account_id" {
+  type         = string
+  description  = "The Account ID for the Auditing Account"
+}
+
+variable "aft_management_account_id" {
+  type        = string
+  description = "The Account ID for the account to hold the AFT automation"
+}
