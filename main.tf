27469e29259617a54ec57a27cb2784573bf58dcb
Christopher Dearie <cdearie@gmail.com>
2023-11-10 09:49:55 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+
+
+module "aft" {
+  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  # Required Vars
+  ct_management_account_id    = var.ct_management_account_id
+  log_archive_account_id      = var.log_archive_account_id
+  audit_account_id            = var.audit_account_id
+  aft_management_account_id   = var.aft_management_account_id
+  ct_home_region              = "us-west-2"
+  tf_backend_secondary_region = "us-east-2"
+  
+  # VCS Vars
+  vcs_provider                                  = "github"
+  account_request_repo_name                     = "chris2fer/aft-account-request"
+  global_customizations_repo_name               = "chris2fer/aft-global-customizations"
+  account_customizations_repo_name              = "chris2fer/aft-account-customizations"
+  account_provisioning_customizations_repo_name = "chris2fer/aft-account-provisioning-customizations"
+
+  # Optional Vars
+  aft_vpc_endpoints = false
+}
